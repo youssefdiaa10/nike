1bf2842db34dbb85dae743ddc43f9daf889eda15
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-25 01:00:44 +0300

    Finish PopularProducts

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+import { useState } from "react";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  // const [showShoes, setShowShoes] = useState(products.imgURL);
+
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red"> Popular </span> Products
+        </h2>
+        <p className="font-montserrat text-slate-gray mt-2 lg:max-w-lg">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
