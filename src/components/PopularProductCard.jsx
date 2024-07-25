55e08e59ab7a267236bf8437e7e0b0130754d367
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-25 17:41:12 +0300

    Change Price font-size from "text-2xl" to "text-[17px]"

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -24,7 +24,7 @@ const PopularProductCard = ({ imgURL, name, price }) => {
         <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
           {name}
         </h3>
-        <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
+        <p className="mt-2 font-semibold font-montserrat text-coral-red text-[17px] leading-normal">
           {price}
         </p>
       </div>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,10 +1,18 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
     <section
       id="about-us"
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
     >
-      SuperQuality
+      <div className="">
+        <h2 className="">
+          <p className=""></p>
+          <button className=""></button>
+        </h2>
+      </div>
     </section>
   );
 };
