c97b010d5e1ea4f50c55869e07d8e359afe91a4e
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-25 17:55:01 +0300

    Finish SuperQuality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -5,13 +5,36 @@ const SuperQuality = () => {
   return (
     <section
       id="about-us"
-      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container "
     >
-      <div className="">
-        <h2 className="">
-          <p className=""></p>
-          <button className=""></button>
+      <div className="flex flex-1 flex-col ">
+        <h2 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+          We Provide You
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality </span>
+          Shoes
         </h2>
+        <p className="mt-4 lg:max-w-lg into-text ">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg into-text ">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-10">
+          <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
