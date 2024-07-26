578f19338e3d55042c424e632088e7970887e206
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-26 20:42:17 +0300

    Finish SpecialOffer section and modify Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red hover:bg-red-700 rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red hover:bg-red-700 text-white border-coral-red"
+      }  rounded-full `}
+    >
       {label}
 
       {iconUrl && (
