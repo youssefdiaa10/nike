211a9bd470dc1999237c6f240c44071b6658d57d
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-26 20:08:36 +0300

    Finish Services section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 const Button = ({ label, iconUrl }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red hover:bg-red-700 rounded-full text-white border-coral-red">
       {label}
 
       {iconUrl && (
         <img
           src={iconUrl}
           alt="arrow-right-icon"
-          className="ml-2 rounded-full w-5 h-5"
+          className="ml-2 rounded-full w-5 h-5 hover:translate-x-1"
         />
       )}
     </button>
