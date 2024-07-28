2fec73b9876c05cd2ce8854439fe0a7cd5aea6b6
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-28 22:32:47 +0300

    Finish Customer Review section

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,14 +94,14 @@ export const services = [
 export const reviews = [
   {
     imgURL: customer1,
-    customerName: "Morich Brown",
+    customerName: "Mike Brown",
     rating: 4.5,
     feedback:
       "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!",
   },
   {
     imgURL: customer2,
-    customerName: "Lota Mongeskar",
+    customerName: "Lota Stark",
     rating: 4.5,
     feedback:
       "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
