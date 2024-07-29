472dc2aeb7ba1958e69640b1476e5a50f5c2d152
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-07-29 22:40:38 +0300

    finish sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  function handleToggle() {
+    setToggle(!toggle);
+  }
   return (
     <header className="padding-x py-8 absolute z-10 w-full ">
       <nav className="flex justify-between items-center max-container">
@@ -16,14 +19,13 @@ const Navbar = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-slate-gray"
+                className="font-montserrat font-bold leading-normal text-slate-gray hover:text-coral-red"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-
         <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
           <a href="/" className="hover:text-coral-red">
             Sign in
@@ -39,8 +41,31 @@ const Navbar = () => {
             src={hamburger}
             alt="hamburger"
             className="w-[28px] h-[28px] object-contain"
-            onClick={() => setToggle((prev) => !prev)}
+            onClick={handleToggle}
           />
+          <div
+            className={`${
+              toggle ? "flex" : "hidden"
+            } p-6 bg-coral-red absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className="list-none flex flex-col justify-end items-center flex-1">
+              {navLinks.map((link, index) => (
+                <li
+                  key={link.label}
+                  className={`font-palanquin font-normal cursor-pointer text-[16px] text-white ${
+                    index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                  }`}
+                >
+                  <a
+                    href={link.href}
+                    className="font-sans text-black hover:text-white"
+                  >
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
